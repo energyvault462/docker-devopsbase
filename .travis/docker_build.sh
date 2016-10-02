5ed506fdafe1c0becafd68088e3a5e5e81d21ca2
Daniel Hess <dan9186@gmail.com>
2016-10-01 23:29:35 -0700

    adding more output details

--- .travis/docker_build.sh
+++ .travis/docker_build.sh
@@ -9,7 +9,15 @@ fi
 if [[ "$GIT_CHANGES" == *"Dockerfile"* || "$DEBUG" == "true" ]]; then
 	echo "Dockerfile changes detected"
 
+	BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+	BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
+	IMAGE_VERSION="0.0.1"
+
 	echo "Testing Dockerfile"
+	echo "		Build Date: $BUILD_DATE"
+	echo "		Build Number: $BUILD_NUMBER"
+	echo "		Image Version: $IMAGE_VERSION"
+
 	docker build \
 		--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 		--build-arg BUILD_NUMBER=$TRAVIS_BUILD_NUMBER \
