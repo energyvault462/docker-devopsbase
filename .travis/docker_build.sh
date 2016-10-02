aec5909e770c92d4cfa8c8a1b3a9ed3c78752feb
Daniel Hess <dan9186@gmail.com>
2016-10-01 23:33:03 -0700

    adding image name to set in travis

--- .travis/docker_build.sh
+++ .travis/docker_build.sh
@@ -14,11 +14,13 @@ if [[ "$GIT_CHANGES" == *"Dockerfile"* || "$DEBUG" == "true" ]]; then
 	IMAGE_VERSION="0.0.1"
 
 	echo "Testing Dockerfile"
+	echo "		Name: $IMAGE_NAME"
 	echo "		Build Date: $BUILD_DATE"
 	echo "		Build Number: $BUILD_NUMBER"
 	echo "		Image Version: $IMAGE_VERSION"
 
 	docker build \
+		-t $IMAGE_NAME \
 		--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 		--build-arg BUILD_NUMBER=$TRAVIS_BUILD_NUMBER \
 		--build-arg VERSION="0.0.1" \
